b94deaf7eb331b43024fc8001d55783c939ecd61
JsonLucas <jasonlucas443@gmail.com>
2022-03-09 17:43:42 -0300

    rebuild-instagram-v1.00

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Body from './components/Body';
+import './style.css';
+
+function App() {
+  return (
+    <div className="corpo">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Stories.js
@@ -0,0 +1,15 @@
+function Stories(username){
+    const srcImage = `assets/img/${username.name}.svg`;
+    return (
+        <div className="story">
+            <div className="imagem">
+                <img src={srcImage} />
+            </div>
+            <div className="usuario">
+                {username.name}
+            </div>
+        </div>
+    );
+}
+
+export default Stories;
